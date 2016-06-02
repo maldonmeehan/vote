0a45028382e205e46ca1899cc004024dc0ff1bd2
Halle Williams and Maldon Meehan <halle@>
2016-06-02 14:25:36 -0700

    add id to form, fix function and parse return on animal, add hide to branch in scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,26 @@
 $(document).ready(function() {
-  $("#animal form").submit(function(event)
-  var animal = $("#animal").val();
+  $("form#selected-animal").submit(function(event){
+
+    $("#animal")
+    var animal = parseInt($("#animal").val());
+  
 
 
-if (animal === 1) {
-  $('#turtle').show();
-} else if (animal === 2) {
-  $('#snake').show();
-} else { (animal === 3)
-  $('#spider').show();
-}
+    if (animal === 1) {
+      $('#turtle').show();
+      $('#snake').hide();
+      $('#spider').hide();
+    } else if (animal === 2) {
+      $('#snake').show();
+      $('#turtle').hide();
+      $('#spider').hide();
+    } else {
+      (animal === 3)
+      $('#spider').show();
+      $('#snake').hide();
+      $('#turtle').hide();
+    }
 
-event.preventDefault();
+    event.preventDefault();
   });
 });
